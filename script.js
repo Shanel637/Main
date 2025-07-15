7ae706ff98e98c5b8d8d222e2bab1e95f6b1d317
Shanel637 <kleekluk@gmail.com>
2025-07-15 19:25:49 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const burgerBtn = document.querySelector(".burger")
+
+function burgerClick() {
+    burgerBtn.classList.toggle(active)
+}
+
+const menu = document.querySelector(".menu")
+
+function menuOp() {
+    menu.classList.toggle("openMenu")
+    console.log("Error")
+
+}
